bbc33049c630c42ef2cf8a2b0df20c2cedfd12fd
isaquerr25 <isaqueribeiro964@gmail.com>
2023-05-16 12:50:51 +0100

    align

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -9,8 +9,13 @@ const ProjectsContainer = () => {
       If you're looking for a technology partner for your success, don't miss the opportunity to check out my portfolio on GitHub. 
       Click on the link below and find out how my skills can turn your ideas into reality!
       </p>
-      <a href="https://github.com/isaquerr25" className="btn">
-        <DiGithubAlt/> <span>See Projects</span>
+      <a href="https://github.com/isaquerr25" className="btn" style={{
+        textAlign: "center",
+        display: "flex",
+        justifyItems: "center",
+        alignItems: "center",
+      }}>
+        <DiGithubAlt/> See Projects
       </a>
     </section>
   );
